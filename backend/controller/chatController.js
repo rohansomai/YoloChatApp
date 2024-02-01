888f85f98e52baebd0792669209ffb3b8e7d980b
Rohan Somai <rohansomai6@gmail.com>
2024-02-02 00:13:28 +0530

    Made all group chat APIs

--- backend/controller/chatController.js
+++ backend/controller/chatController.js
@@ -4,10 +4,8 @@ const User = require('../models/User');
 
 const accessChat = expressAsyncHandler(async (req, res) => {
   const { userId } = req.body; // receiver's userId
-
   if (!userId) {
-    console.log('UserId param not sent with request');
-    return res.sendStatus(400);
+    return res.status(400).send({ message: 'UserId param not sent with request' });
   }
   let isChatPresent = await Chat.find({
     isGroupChat: false,
@@ -16,10 +14,10 @@ const accessChat = expressAsyncHandler(async (req, res) => {
     .populate('users', '-password')
     .populate('latestMessage');
 
-  // isChatPresent = await User.populate(isChatPresent, {
-  //   path: 'latestMessage.sender',
-  //   select: 'name pic email',
-  // });
+  isChatPresent = await User.populate(isChatPresent, {
+    path: 'latestMessage.sender',
+    select: 'name pic email',
+  });
 
   if (isChatPresent.length > 0) {
     res.send(isChatPresent[0]);
@@ -53,7 +51,101 @@ const fetchChats = expressAsyncHandler(async (req, res) => {
     res.status(200).json(userChats);
   } catch (error) {
     console.error(error);
+    res.status(500).json({ message: 'Something went wrong while fetching chats' });
+    throw new Error('Something went wrong while fetching chats');
   }
 });
 
-module.exports = { accessChat, fetchChats };
+const createGroupChat = expressAsyncHandler(async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: 'Please provide all the fields' });
+  }
+  const users = JSON.parse(req.body.users);
+  if (users.length < 2) {
+    return res.status(400).send({ message: 'More than two users required to form a group chat' });
+  }
+  users.push(req.user);
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      isGroupChat: true,
+      users: users,
+      groupAdmin: req.user,
+    });
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+    res.status(201).json(fullGroupChat);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong while creating group chat' });
+    throw new Error('Something went wrong while creating group chat');
+  }
+});
+
+const renameGroup = expressAsyncHandler(async (req, res) => {
+  if (!req.body.chatName || !req.body.groupChatId) {
+    return res.status(400).send({ message: 'Please provide all the fields' });
+  }
+  try {
+    const { groupChatId, chatName } = req.body;
+    const updatedChat = await Chat.findOneAndUpdate({ _id: groupChatId }, { chatName }, { new: true })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+    if (!updatedChat) {
+      res.status(404).json({ message: 'Chat not found' });
+    } else {
+      res.status(200).json(updatedChat);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong while updating group chat' });
+    throw new Error('Something went wrong while updating group chat');
+  }
+});
+
+const addToGroup = expressAsyncHandler(async (req, res) => {
+  try {
+    const { groupChatId, userId } = req.body;
+    if (!groupChatId || !userId) {
+      return res.status(400).send({ message: 'Please provide all the fields' });
+    }
+    const chatDetails = await Chat.findOne({ _id: groupChatId }).select('users');
+    if (!chatDetails) {
+      return res.status(404).json({ message: 'Chat not found' });
+    }
+    if (chatDetails.users.includes(userId)) {
+      return res.status(400).json({ message: 'This user already exists in this group' });
+    }
+    const updatedChat = await Chat.findOneAndUpdate({ _id: groupChatId }, { $push: { users: userId } }, { new: true })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+    return res.status(200).json(updatedChat);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong while updating group chat' });
+    throw new Error('Something went wrong while updating group chat');
+  }
+});
+
+const removeFromGroup = expressAsyncHandler(async (req, res) => {
+  const { groupChatId, userId } = req.body;
+  if (!groupChatId || !userId) {
+    return res.status(400).json({ message: 'Please provide all the fields' });
+  }
+  try {
+    const updatedChat = await Chat.findOneAndUpdate({ _id: groupChatId }, { $pull: { users: userId } }, { new: true })
+      .populate('users', '-password')
+      .populate('groupAdmin', '-password');
+    if (!updatedChat) {
+      return res.status(404).json({ message: 'Chat not found' });
+    }
+    res.status(200).json(updatedChat);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Something went wrong while updating group chat' });
+    throw new Error('Something went wrong while updating group chat');
+  }
+});
+
+module.exports = { accessChat, fetchChats, createGroupChat, renameGroup, addToGroup, removeFromGroup };
--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const { protect } = require('../middlewares/auth.middleware');
-const { accessChat, fetchChats } = require('../controller/chatController');
+const {
+  accessChat,
+  fetchChats,
+  createGroupChat,
+  renameGroup,
+  addToGroup,
+  removeFromGroup,
+} = require('../controller/chatController');
 
 const router = express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
-// router.route("/group").post(protect, createGroupChat);
-// router.route("/rename").put(protect, renameGroup);
-// router.route("/groupremove").put(protect, removeFromGroup);
-// router.route("/groupadd").put(protect, addToGroup);
+router.route('/group').post(protect, createGroupChat);
+router.route('/group/rename').put(protect, renameGroup);
+router.route('/group/add-user').put(protect, addToGroup);
+router.route('/group/remove-user').put(protect, removeFromGroup);
 
 module.exports = router;
